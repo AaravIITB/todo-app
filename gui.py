0b4f0b5e6fe4f3e3fe59e9cae975a0ff694f3f07
aaravmaniyar <aaraviniit@gmail.com>
2024-12-10 21:52:02 +0530

    Change theme, add clock, handle edit and complete error

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import function
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("BlueMono")
+
+label_time= sg.Text("",key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
@@ -11,7 +15,8 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[label_time],
+                           [label],
                            [input_box,add_button],
                            [list_box,edit_button,complete_button],
                            [exit_button]],
@@ -19,9 +24,8 @@ window = sg.Window('My To-Do App',
 
 
 while True:
-    event,values = window.read()
-    print(1,event)
-    print(2,values)
+    event,values = window.read(timeout=200)#loop runs every 10 milliseconds
+    window["clock"].update(value=time.strftime("%b %d,%Y %H:%M:%S"))
 
     match event:
         case "Add":
@@ -32,21 +36,26 @@ while True:
             window['todos'].update(values=todos)#updates the window in real time
 
         case "Edit":
-            todo_to_edit = values['todos'][0]#to get the string
-            new_todo = values['todo'] + "\n"
-            todos = function.get_todo()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            function.write_todo(todos)
-            window['todos'].update(values=todos)#updates the window in real time
-
+            try:
+                todo_to_edit = values['todos'][0]#to get the string
+                new_todo = values['todo'] + "\n"
+                todos = function.get_todo()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                function.write_todo(todos)
+                window['todos'].update(values=todos)#updates the window in real time
+            except IndexError:
+                sg.popup("Please select a task",font=("Helvetica",20))
         case "Complete":
-            todo_to_complete=values['todos'][0]
-            todos = function.get_todo()
-            todos.remove(todo_to_complete)
-            function.write_todo(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete=values['todos'][0]
+                todos = function.get_todo()
+                todos.remove(todo_to_complete)
+                function.write_todo(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select a task",font=("Helvetica",20))
 
         case "Exit":
             break
